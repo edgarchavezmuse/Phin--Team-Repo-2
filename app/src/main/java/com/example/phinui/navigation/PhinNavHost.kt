ea85083e7f386cbb73d0b80890c5fd39ef52ce30
David <david.villicana017@myci.csuci.edu>
2026-03-14 21:15:22 -0700

    feat: add functionality for adding events to calendar - in progress

--- app/src/main/java/com/example/phinui/navigation/PhinNavHost.kt
+++ app/src/main/java/com/example/phinui/navigation/PhinNavHost.kt
@@ -1,6 +1,9 @@
 package com.example.phinui.ui.navigation
 
+import android.widget.Toast
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -9,11 +12,16 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.phinui.data.calendar.CalendarEvent
+import com.example.phinui.data.calendar.CalendarStorage
 import com.example.phinui.screens.CalendarScreen
 import com.example.phinui.ui.screens.EventsScreen
 import com.example.phinui.ui.screens.FavoritesScreen
 import com.example.phinui.ui.screens.HomeScreen
 import com.example.phinui.ui.screens.ProfileScreen
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 @Composable
 fun PhinNavHost(
@@ -44,13 +52,47 @@ fun PhinNavHost(
 
             // for storing the event in calendar - persistence
             val currentContext = LocalContext.current
+
+            // for event to stay persistent in calendar
+            val storeEvent = CalendarStorage(currentContext)
+
+            // display saved events in calendar
+            LaunchedEffect(Unit) {
+                val loadedEvents = storeEvent.loadEvents()
+                savedEvents.addAll(loadedEvents)
+            }
+
             EventsScreen(onEventClick = { event ->
-                // test dialog box functionality
-                if (savedEvents.none { it.id == event.id}) {
-                    savedEvents.add(event)
+                // Show 'add to calendar?' message
+                CoroutineScope(Dispatchers.IO).launch {
+
+                    // check if clicked event already exists in calendar
+                    if (savedEvents.any { it.id == event.id }) {
+                        withContext(Dispatchers.Main){
+                            Toast.makeText(
+                                currentContext,
+                                "${event.title} already in your calendar.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    } else {
+                        // stores event in calendar and is persistent
+                        storeEvent.saveEvent(event)
+
+                        // updates list of events in calendar
+                        savedEvents.add(event)
+
+                        // confirmation message
+                        withContext(Dispatchers.Main) {
+                            Toast.makeText(
+                                currentContext,
+                                "${event.title} added to your calendar.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
                 }
-                // Show 'add to calendar' message
-            })
+            } )
         }
 
         composable(Routes.CALENDAR) {
