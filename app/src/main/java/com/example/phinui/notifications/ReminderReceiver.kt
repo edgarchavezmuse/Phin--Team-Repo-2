e572f3d49533d2392f2daa72808f891b9a0beafe
marzort <maria.ortiz559@myci.csuci.edu>
2026-03-23 21:28:36 -0700

    polished up reminder ui

--- app/src/main/java/com/example/phinui/notifications/ReminderReceiver.kt
+++ app/src/main/java/com/example/phinui/notifications/ReminderReceiver.kt
@@ -3,12 +3,26 @@ package com.example.phinui.notifications
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import com.example.phinui.notifications.NotificationHelper.sendNotificationsForCalendar
 
 // works with Alarm Manager to allow for notifications even when app is closed
 class ReminderReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
-        val title = intent.getStringExtra("title") ?: return
+        Log.d("ReminderTitleCheck", "intent.getStringExtra value: " + intent.getStringExtra("title"))
+
+        val titleTester = intent.getStringExtra("title")
+        val title =
+            if (titleTester != null) {
+                if (titleTester == "(No title)") {
+                    null
+                } else {
+                    titleTester
+                }
+            } else {
+                return
+            }
+
         val eventId = intent.getStringExtra("eventId") ?: return
 
         sendNotificationsForCalendar(context, title, eventId)
