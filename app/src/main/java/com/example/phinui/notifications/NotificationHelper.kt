6d3a5b9c11450c851f419534e21e68384f1e1e6d
marzort <maria.ortiz559@myci.csuci.edu>
2026-03-26 12:59:01 -0700

    calendar notifications now navigate to calendar screen

--- app/src/main/java/com/example/phinui/notifications/NotificationHelper.kt
+++ app/src/main/java/com/example/phinui/notifications/NotificationHelper.kt
@@ -7,6 +7,7 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
@@ -40,10 +41,16 @@ object NotificationHelper {
             manager.createNotificationChannel(channel)
         }
 
+        val intent =
+            Intent(Intent.ACTION_VIEW, Uri.parse("phin://calendar")).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
+
+        // changed Intent(context, MainActivity::class.java) to intent
         val pendingIntent = PendingIntent.getActivity(
             context,
             0,
-            Intent(context, MainActivity::class.java),
+            intent,
             PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
         )
 
