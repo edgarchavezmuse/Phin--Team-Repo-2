06b42a048d714bc9e8cd23e97a1d69e0b08ee9fb
David <david.villicana017@myci.csuci.edu>
2026-04-04 23:10:15 -0700

    feat: add back to list button on MessagesScreen

--- app/src/main/java/com/example/phinui/screens/MessagesScreen.kt
+++ app/src/main/java/com/example/phinui/screens/MessagesScreen.kt
@@ -1,34 +1,31 @@
 package com.example.phinui.ui.screens
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.graphics.Color
 import androidx.compose.runtime.*
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.Dp
+import androidx.navigation.NavController
 import com.example.phinui.ui.theme.Background
 import com.example.phinui.ui.theme.NavText
 import com.example.phinui.components.messages.ChatRepository
+import com.example.phinui.ui.navigation.Routes
 import com.example.phinui.ui.theme.*
-import com.google.android.libraries.places.api.model.kotlin.rectangularBounds
+
 
 @Composable
-fun MessagesScreen(senderUserID: String, receiverUserID: String) {
+fun MessagesScreen(senderUserID: String, receiverUserID: String, navController: NavController) {
     val chatRepository = remember { ChatRepository() }
     var messageText by remember { mutableStateOf("") }
     var messages by remember { mutableStateOf(listOf<Map<String, Any>>()) }
@@ -41,6 +38,17 @@ fun MessagesScreen(senderUserID: String, receiverUserID: String) {
     }
 
     Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.End,
+        ) {
+            Button(onClick = {
+                navController.navigate(Routes.USERLIST)
+            }
+            ) {
+                Text ("Back to list")
+            }
+        }
         LazyColumn(
             modifier = Modifier.weight(1f),
             reverseLayout = false,
@@ -75,6 +83,7 @@ fun MessagesScreen(senderUserID: String, receiverUserID: String) {
                 }
             }
         }
+
         Row(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.spacedBy(8.dp),
