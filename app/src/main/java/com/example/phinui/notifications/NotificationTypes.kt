0185dec341f11dc0cf68088f60128b3cac038337
marzort <maria.ortiz559@myci.csuci.edu>
2026-04-16 17:12:10 -0700

    added notifications for accepted friend request

--- app/src/main/java/com/example/phinui/notifications/NotificationTypes.kt
+++ app/src/main/java/com/example/phinui/notifications/NotificationTypes.kt
@@ -16,7 +16,7 @@ enum class NotificationType(
 
     FRIEND_REQUESTS(
         "friend_request_channel",
-        "Friend Requests V2",
+        "Friend Requests V3",
         NotificationManager.IMPORTANCE_HIGH
     )
 }
