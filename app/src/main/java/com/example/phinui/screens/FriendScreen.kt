d5dfc11fbb1e425d73f77559d29e2636fc7ed51e
s-Saldivar <samuel.saldivar677@myci.csuci.edu>
2026-04-20 17:53:32 -0700

    - users get a pop window saying they already have a friend request pending

--- app/src/main/java/com/example/phinui/screens/FriendScreen.kt
+++ app/src/main/java/com/example/phinui/screens/FriendScreen.kt
@@ -157,6 +157,13 @@ fun FriendsScreen(
                         onClick = { selectedTab = 1 }
                     ) {
                         Text("Requests")
+//                        Text(
+//                            if (incoming.isNotEmpty()) {
+//                                "Requests (${incoming.size})"
+//                            } else {
+//                                "Requests"
+//                            }
+//                        )
                     }
                 }
 
