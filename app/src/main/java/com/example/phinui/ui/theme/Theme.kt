306af62ecfc525dd4b3f3f133a4f4408a4c75b2f
s-Saldivar <samuel.saldivar677@myci.csuci.edu>
2026-04-28 23:09:16 -0700

    - dark mode slightly implemented - need to fix hard coded colors

--- app/src/main/java/com/example/phinui/ui/theme/Theme.kt
+++ app/src/main/java/com/example/phinui/ui/theme/Theme.kt
@@ -50,6 +50,21 @@ private val DarkColorScheme = darkColorScheme(
     onSurface = DarkTextPrimary
 )
 
+private val DarkColorScheme = darkColorScheme(
+    primary = DarkHeaderRed,
+    secondary = DarkTextSecondary,
+    tertiary = DarkSelectedPill,
+
+    background = DarkBackground,
+    surface = DarkSurface,
+
+    onPrimary = DarkOnPrimary,
+    onSecondary = DarkTextPrimary,
+    onTertiary = DarkTextPrimary,
+    onBackground = DarkTextPrimary,
+    onSurface = DarkTextPrimary
+)
+
 @Composable
 fun PhinUITheme(
     darkMode: Boolean = false,
