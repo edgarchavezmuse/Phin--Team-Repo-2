d0e4a87d3f6f034f7102f009efc64f6da44c94d9
s-Saldivar <samuel.saldivar677@myci.csuci.edu>
2026-04-28 23:09:16 -0700

    - dark mode slightly implemented - need to fix hard coded colors

--- app/src/main/java/com/example/phinui/MainActivity.kt
+++ app/src/main/java/com/example/phinui/MainActivity.kt
@@ -218,7 +218,7 @@ fun PhinUIApp(
                         )
                     )
                 },
-                containerColor = MaterialTheme.colorScheme.surface,
+                containerColor = MaterialTheme.colorScheme.background,
                 bottomBar = {
                     when (bottomBarType) {
 
