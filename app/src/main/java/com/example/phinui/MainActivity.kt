a1765220582e04db2062298772e0d58499195b7a
s-Saldivar <samuel.saldivar677@myci.csuci.edu>
2026-05-03 22:01:40 -0700

    - turned on waves in Login and Register screen

--- app/src/main/java/com/example/phinui/MainActivity.kt
+++ app/src/main/java/com/example/phinui/MainActivity.kt
@@ -100,7 +100,7 @@ fun PhinUIApp(
         currentRoute == Routes.LOGIN || currentRoute == Routes.REGISTER
     val isInMessages = currentRoute?.startsWith(Routes.MESSAGES) == true
 
-    val showAuthWaves = false // turns waves and ship on and off in Login and Register screens
+    val showAuthWaves = true // turns waves and ship on and off in Login and Register screens
     val waveAnimation = rememberWaveAnimationState(
         frontDurationMillis = 4200, // speed of wave
         backDurationMillis = 3500, // speed of wave
