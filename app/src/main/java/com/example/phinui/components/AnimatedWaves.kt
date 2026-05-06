d84be139a84b447a0289aa4eebb56cfbe10c7abe
s-Saldivar <samuel.saldivar677@myci.csuci.edu>
2026-05-05 18:17:42 -0700

    - credit screen

--- app/src/main/java/com/example/phinui/components/AnimatedWaves.kt
+++ app/src/main/java/com/example/phinui/components/AnimatedWaves.kt
@@ -48,7 +48,7 @@ fun AnimatedWaves(
     shipProgress: Float,
     bobPhase: Float
 ) {
-    val labelColor = MaterialTheme.colorScheme.onTertiary
+    val labelColor = MaterialTheme.colorScheme.onBackground
 
     Canvas(
         modifier = modifier
