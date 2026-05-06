3cc74b88220b65c5d688bab8d68387ea8172b03c
s-Saldivar <samuel.saldivar677@myci.csuci.edu>
2026-05-05 18:17:42 -0700

    - credit screen

--- app/src/main/java/com/example/phinui/navigation/Routes.kt
+++ app/src/main/java/com/example/phinui/navigation/Routes.kt
@@ -21,6 +21,7 @@ object Routes {
     const val FRIENDS = "friends"
     const val PEOPLE = "people"
     const val SETTINGS = "settings"
+    const val CREDITS = "credits"
     fun mapRouteWithPin(
         pinId: String,
         pinName: String,
